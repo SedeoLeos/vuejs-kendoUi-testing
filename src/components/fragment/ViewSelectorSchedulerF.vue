8c837165e1f1733ec5db9025d225c0c1adfc5058
Gedeon <smatsoula19@gmail.com>
2023-03-24 13:05:04 +0100

    first commit

---
+++ src/components/fragment/ViewSelectorSchedulerF.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="viewSelectorScheduler">
+        <button v-for="item in views" :key="view" :class="item.name === view ? 'active' : ''" :selected="item.name === view"
+            @click="changeView(item)"> {{ item.title }}</button>
+    </div>
+</template>
+<style>
+div.viewSelectorScheduler {
+    justify-self: center;
+    width: 300px;
+    height: 30px;
+    display: flex;
+    background-color: rgb(215, 215, 215);
+    border-radius: 20px;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0;
+    padding: 0;
+}
+
+div.viewSelectorScheduler button {
+    border: none;
+    background-color: transparent;
+    font-weight: 400;
+    padding-left: 20px;
+    padding-right: 20px;
+    height: 100%;
+}
+
+div.viewSelectorScheduler button.active {
+    color: #fff;
+    background-color: blueviolet;
+    border-radius: 20px;
+}
+</style>
+<script lang="ts">
+export default {
+    props: {
+        view: {
+            type: String,
+            required: true
+        },
+        views: { type: Array<{ name: String, title: String }>, required: true },
+    },
+    methods: {
+        changeView(view: { name: String, title: String }) {
+            this.$emit("view-changed", view);
+        }
+    }
+
+}
+</script>
